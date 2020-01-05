ec085015f22bcbfcfdae2d3cd169c1b1c0001616
Ponta5578 <59057488+Ponta5578@users.noreply.github.com>
2020-01-05 21:36:05 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
-class user < ApplicationRecord
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :messeages
   has_many :groups_users
   has_many :groups, through: :groups_users
